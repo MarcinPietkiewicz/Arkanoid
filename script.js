d6c4d8df3aaa711b7ef67239f3bfb1880b39f472
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-06-21 19:59:48 +0200

    Added ball movement

---
+++ script.js
@@ -0,0 +1,38 @@
+var ballX = 75;
+var ballY = 75;
+var ballSpeedX = 5;
+var ballSpeedY = 8;
+
+var canvas, canvasContext;
+
+window.onload = function() {
+  canvas = document.getElementById("gameCanvas");
+  canvasContext = canvas.getContext("2d");
+
+  var framesPerSecond = 30;
+  setInterval(updateAll, 1000 / framesPerSecond);
+};
+
+function updateAll() {
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  if (ballX > canvas.width) {
+    ballSpeedX *= -1;
+  }
+  if (ballX < 0) {
+    ballSpeedX *= -1;
+  }
+  if (ballY > canvas.height) {
+    ballSpeedY *= -1;
+  }
+  if (ballY < 0) {
+    ballSpeedY *= -1;
+  }
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = "red";
+  canvasContext.beginPath();
+  canvasContext.arc(ballX, ballY, 10, 0, Math.PI * 2, true);
+  canvasContext.fill();
+}
